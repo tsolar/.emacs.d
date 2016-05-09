296619c790f59806897e3cd374594fa6a9158a0b
Tomás Solar Castro <tsolar@gmail.com>
2016-05-09 11:12:52 -0300

    using sml dark theme and another minor customs

--- lisp/my-sml.el
+++ lisp/my-sml.el
@@ -1,6 +1,15 @@
 (setq sml/no-confirm-load-theme t)
+
+(display-time-mode)
+(setq powerline-arrow-shape 'curve)
+(setq powerline-default-separator-dir '(right . left))
+(setq sml/theme 'powerline)
+(setq sml/mode-width 0)
+(setq sml/name-width 20)
+(rich-minority-mode 1)
+(setf rm-blacklist "")
 (sml/setup)
 
-(sml/apply-theme 'powerline)
+(sml/apply-theme 'dark)
 
 (provide 'my-sml)
