35b2a2015fbdf0c8f67bb5646d889984ca18b21c
Tomás Solar Castro <tsolar@gmail.com>
2016-06-14 12:49:31 -0400

    added modes to projectile-tramp workaround

--- lisp/my-projectile.el
+++ lisp/my-projectile.el
@@ -2,6 +2,9 @@
 ;; workaround for tramp: (see https://github.com/bbatsov/prelude/issues/594)
 (add-hook 'text-mode-hook #'projectile-mode)
 (add-hook 'prog-mode-hook #'projectile-mode)
+(add-hook 'magit-mode-hook #'projectile-mode)
+(add-hook 'gitignore-mode-hook #'projectile-mode)
+
 
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
