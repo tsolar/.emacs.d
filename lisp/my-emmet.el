d70ae32a41e5355729686bc473a5ac6254ede7aa
Tomás Solar Castro <tsolar@gmail.com>
2016-06-14 13:15:19 -0400

    emmet in web-mode

--- lisp/my-emmet.el
+++ lisp/my-emmet.el
@@ -2,6 +2,7 @@
 
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook  'emmet-mode) ;; enable Emmet in web-mode
 
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 (setq emmet-expand-jsx-className? t) ;; default nil
