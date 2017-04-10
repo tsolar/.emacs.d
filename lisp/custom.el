1fe6bd0a2d379a930cc29a4050986f0f40b1c498
Tomás Solar Castro <tsolar@gmail.com>
2017-04-10 16:53:49 -0300

    lighter linux foreground

--- lisp/custom.el
+++ lisp/custom.el
@@ -118,6 +118,7 @@
  '(helm-buffer-process ((t (:foreground "yellow"))))
  '(highlight ((t (:background nil))))
  '(iedit-occurrence ((t (:background "gold" :foreground "black"))))
+ '(linum ((t (:foreground "dark gray"))))
  '(show-paren-match ((t (:background nil :foreground "gold" :weight bold))))
  '(sp-show-pair-match-face ((t (:inherit show-paren-match :background nil))))
  '(sp-show-pair-mismatch-face ((t (:inherit show-paren-mismatch :background nil :foreground "white"))))
