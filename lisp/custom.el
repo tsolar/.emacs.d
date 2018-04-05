f95fdc0b4ffcc679f1ba84c196da6801565e1f1e
Tomás Solar Castro <tsolar@users.noreply.github.com>
2018-04-05 11:56:20 -0300

    ag settings
    set to t:
    * ag-reuse-window
    * ag-reuse-buffer
    * ag-highlight-search

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t)
+ '(ag-reuse-buffers t)
+ '(ag-reuse-window t)
  '(blink-cursor-mode nil)
  '(c-tab-always-indent nil)
  '(column-number-mode t)
